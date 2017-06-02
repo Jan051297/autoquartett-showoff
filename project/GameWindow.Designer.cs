37afa4115bb8955a95cc8ae40c50c79b7436321e
Jan Schmitter <jan.schmitter@gmail.com>
2017-06-02 09:49:28 +0200

    UI: Improved Layout

--- project/GameWindow.Designer.cs
+++ project/GameWindow.Designer.cs
@@ -28,72 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelViewCards = new System.Windows.Forms.Panel();
+            this.listCards = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.cardPanelLeft = new project.QuartetCardPanel();
             this.cardPanelRight = new project.QuartetCardPanel();
-            this.listCards = new System.Windows.Forms.FlowLayoutPanel();
-            this.panelViewCards.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panelViewCards
-            // 
-            this.panelViewCards.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelViewCards.Controls.Add(this.cardPanelRight);
-            this.panelViewCards.Controls.Add(this.cardPanelLeft);
-            this.panelViewCards.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelViewCards.Location = new System.Drawing.Point(0, 386);
-            this.panelViewCards.MinimumSize = new System.Drawing.Size(0, 300);
-            this.panelViewCards.Name = "panelViewCards";
-            this.panelViewCards.Size = new System.Drawing.Size(834, 300);
-            this.panelViewCards.TabIndex = 1;
-            // 
-            // cardPanelLeft
-            // 
-            this.cardPanelLeft.BackColor = System.Drawing.Color.White;
-            this.cardPanelLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.cardPanelLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.cardPanelLeft.Location = new System.Drawing.Point(0, 0);
-            this.cardPanelLeft.Name = "cardPanelLeft";
-            this.cardPanelLeft.Size = new System.Drawing.Size(250, 298);
-            this.cardPanelLeft.TabIndex = 0;
-            // 
-            // cardPanelRight
-            // 
-            this.cardPanelRight.BackColor = System.Drawing.Color.White;
-            this.cardPanelRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.cardPanelRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.cardPanelRight.Location = new System.Drawing.Point(582, 0);
-            this.cardPanelRight.Name = "cardPanelRight";
-            this.cardPanelRight.Size = new System.Drawing.Size(250, 298);
-            this.cardPanelRight.TabIndex = 1;
             // 
             // listCards
             // 
             this.listCards.AutoScroll = true;
+            this.listCards.AutoSize = true;
+            this.listCards.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.listCards.BackColor = System.Drawing.Color.LightSteelBlue;
             this.listCards.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listCards.Location = new System.Drawing.Point(0, 0);
+            this.listCards.MinimumSize = new System.Drawing.Size(50, 50);
             this.listCards.Name = "listCards";
-            this.listCards.Size = new System.Drawing.Size(834, 386);
-            this.listCards.TabIndex = 2;
+            this.listCards.Size = new System.Drawing.Size(834, 361);
+            this.listCards.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.cardPanelLeft, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cardPanelRight, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 361);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(0, 300);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(834, 300);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // cardPanelLeft
+            // 
+            this.cardPanelLeft.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cardPanelLeft.BackColor = System.Drawing.Color.White;
+            this.cardPanelLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPanelLeft.Location = new System.Drawing.Point(83, 3);
+            this.cardPanelLeft.Name = "cardPanelLeft";
+            this.cardPanelLeft.Size = new System.Drawing.Size(250, 350);
+            this.cardPanelLeft.TabIndex = 0;
+            // 
+            // cardPanelRight
+            // 
+            this.cardPanelRight.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cardPanelRight.BackColor = System.Drawing.Color.White;
+            this.cardPanelRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cardPanelRight.Location = new System.Drawing.Point(500, 3);
+            this.cardPanelRight.Name = "cardPanelRight";
+            this.cardPanelRight.Size = new System.Drawing.Size(250, 350);
+            this.cardPanelRight.TabIndex = 1;
             // 
             // GameWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(834, 686);
+            this.ClientSize = new System.Drawing.Size(834, 661);
             this.Controls.Add(this.listCards);
-            this.Controls.Add(this.panelViewCards);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "GameWindow";
-            this.panelViewCards.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.FlowLayoutPanel listCards;
-        public System.Windows.Forms.Panel panelViewCards;
-        public QuartetCardPanel cardPanelRight;
-        public QuartetCardPanel cardPanelLeft;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private QuartetCardPanel cardPanelLeft;
+        private QuartetCardPanel cardPanelRight;
     }
 }
